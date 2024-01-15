68865f89617d0951c44a5dbd84010ae8a746d82e
legio <kirillnosenko304@gmail.com>
2024-01-15 21:06:08 +0200

    [US3] Initialize SpringBoot app.

---
+++ src/main/java/com/socialnetwork/socialnetworkapi/SocialNetworkApiApplication.java
@@ -0,0 +1,13 @@
+package com.socialnetwork.socialnetworkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetworkApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/socialnetwork/socialnetworkapi/SocialNetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialnetwork.socialnetworkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
