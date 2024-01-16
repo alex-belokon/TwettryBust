1ae73433023a3c4b2708dea4d192076d13281463
Anna Matveeva <107624838+Matveeva-Ann@users.noreply.github.com>
2024-01-16 16:28:24 +0200

    Fixed in style using stylelint

--- frontend/src/components/Buttons/ModalBtn/ModalBtn.jsx
+++ frontend/src/components/Buttons/ModalBtn/ModalBtn.jsx
@@ -10,5 +10,5 @@ export default function ModalBtn({children}){
 }
 
 ModalBtn.propTypes = {
-  children: PropTypes.string,
+  children: PropTypes.node.isRequired,
 };
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import Logo from '../Logo/Logo';
 import './header.style.scss';
 import Navigation from './Navigation/Navigation';
 
-export default function Header() {
+export default function Header(){
 
   return(
     <header className="header">
