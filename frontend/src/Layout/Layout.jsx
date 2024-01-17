6ef07572ef3349ac2193f7eb92e587f80c32a82f
Anna Matveeva <107624838+Matveeva-Ann@users.noreply.github.com>
2024-01-17 13:55:46 +0200

    Us21 (#8)
    * created header, integrated fonts and implemented routes
    
    * created btn
    
    * Fixed in style using stylelint
    
    * Fixed in style using stylelint
    
    * Fixed in style using stylelint
    
    * Fixed in style using stylelint
    
    * Fixed in style using stylelint
    
    * added styles
    
    * changed styles
    
    * changed styles
    
    * changed styles
    
    * changed styles
    
    * added modal
    
    * changed styles
    
    * changed styles
    
    * changed styles
    
    ---------
    
    Co-authored-by: Andrii <126251484+Andrew3552@users.noreply.github.com>

--- frontend/src/Layout/Layout.jsx
+++ frontend/src/Layout/Layout.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import Header from "../components/Header/Header";
 import './layout.style.scss'
 
@@ -5,6 +6,9 @@ export default function Layout() {
   return (
     <div className="wrapper">
       <Header></Header>
+      <main className="mainStyle">
+        <Outlet></Outlet>
+      </main>
     </div>
   );
 }
