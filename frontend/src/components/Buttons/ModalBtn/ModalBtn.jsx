bdc3b9e7d8b10919bb7b954bf26f9ae5014216e9
Anna Matveeva <107624838+Matveeva-Ann@users.noreply.github.com>
2024-01-17 13:55:46 +0200

    Us21 (#8)
    * created header, integrated fonts and implemented routes
    
    * created btn
    
    * Fixed in style using stylelint
    
    * Fixed in style using stylelint
    
    * Fixed in style using stylelint
    
    * Fixed in style using stylelint
    
    * Fixed in style using stylelint
    
    * added styles
    
    * changed styles
    
    * changed styles
    
    * changed styles
    
    * changed styles
    
    * added modal
    
    * changed styles
    
    * changed styles
    
    * changed styles
    
    ---------
    
    Co-authored-by: Andrii <126251484+Andrew3552@users.noreply.github.com>

--- frontend/src/components/Buttons/ModalBtn/ModalBtn.jsx
+++ frontend/src/components/Buttons/ModalBtn/ModalBtn.jsx
@@ -1,12 +1,20 @@
-import PropTypes from 'prop-types';
-import './modalBtn.style.scss';
-export default function ModalBtn({children}){
-
-  return(
-    <button className='modalBtn' type="button">{children}</button>
-  )
+import PropTypes from "prop-types";
+import "./modalBtn.style.scss";
+export default function ModalBtn({ children, ariaLabel, btnClick }) {
+  return (
+    <button
+      className="modalBtn"
+      onClick={btnClick}
+      type="button"
+      aria-label={ariaLabel}
+    >
+      {children}
+    </button>
+  );
 }
 
 ModalBtn.propTypes = {
   children: PropTypes.node.isRequired,
+  ariaLabel: PropTypes.string.isRequired,
+  btnClick: PropTypes.func,
 };
