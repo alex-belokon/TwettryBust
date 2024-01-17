eec66f958909ff0bb39b99b122d6a475e82c6866
Andrii <126251484+Andrew3552@users.noreply.github.com>
2024-01-17 15:09:47 +0200

    Us14 (#9)
    * Initialize React app
    
    * authorization page
    
    * authorization page
    
    * authorization page

--- frontend/src/AppRoutes.jsx
+++ frontend/src/AppRoutes.jsx
@@ -1,23 +1,29 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./Layout/Layout";
 import Home from "./page/Home";
-
+import Authorization from "./page/Authorization"; // Предполагаем, что у вас есть компонент для страницы логина
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route element={<Layout />}>
-        {/* Тестово додала скрізь Home */}
-        <Route index element={<Home />}/>
-        <Route path='explore' element={<Home />}/> 
-        <Route path='notifications' element={<Home />}/>
-        <Route path='messages' element={<Home />}/>
-        <Route path='lists' element={<Home />}/>
-        <Route path='bookmarks' element={<Home />}/>
-        <Route path='communities' element={<Home />}/>
-        <Route path='profile' element={<Home />}/>
-        <Route path='settings' element={<Home />}/>
-      </Route>
+      <Route path="/authorization" element={<Authorization />} />
+      <Route
+        path="/"
+        element={
+          <Layout>
+            {/* Тестово додала скрізь Home */}
+            <Route index element={<Home />} />
+            <Route path="explore" element={<Home />} />
+            <Route path="notifications" element={<Home />} />
+            <Route path="messages" element={<Home />} />
+            <Route path="lists" element={<Home />} />
+            <Route path="bookmarks" element={<Home />} />
+            <Route path="communities" element={<Home />} />
+            <Route path="profile" element={<Home />} />
+            <Route path="settings" element={<Home />} />
+          </Layout>
+        }
+      />
     </Routes>
   );
 }
