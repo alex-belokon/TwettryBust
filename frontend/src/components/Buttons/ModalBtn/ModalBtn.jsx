ac6ca78947b3069e21a22595db35afff34af5f55
Andrew3552 <armbrustrututu66@gmail.com>
2024-01-22 15:15:10 +0200

    LogIn

--- frontend/src/components/Buttons/ModalBtn/ModalBtn.jsx
+++ frontend/src/components/Buttons/ModalBtn/ModalBtn.jsx
@@ -1,11 +1,15 @@
 import PropTypes from "prop-types";
 import "./modalBtn.style.scss";
-export default function ModalBtn({ children, ariaLabel, btnClick }) {
+export default function ModalBtn({ children, ariaLabel, btnClick, additionalClass }) {
+
+  const btnClasses = `modalBtn ${additionalClass || ''}`;
+
   return (
     <button
-      className="modalBtn"
+      // className="modalBtn"
+      className={btnClasses}
       onClick={btnClick}
-      type="button"
+      type="submit"
       aria-label={ariaLabel}
     >
       {children}
--- frontend/src/components/Modal/ModalElements/ModalWrapper.jsx
+++ frontend/src/components/Modal/ModalElements/ModalWrapper.jsx
@@ -3,9 +3,10 @@ import "./modalElements.style.scss";
 import { RxCross2 } from "react-icons/rx";
 import PropTypes from "prop-types";
 import { useTranslation } from "react-i18next";
+import cx from "classnames";
 
 
-export default function ModalWrapper({closeModal, isModalPost=false, children}) {
+export default function ModalWrapper({closeModal, isModalPost=false, children, modalBodyLogIn=false}) {
   const modalRoot = document.getElementById("modal-root");
   const { t } = useTranslation();
 
@@ -17,7 +18,7 @@ export default function ModalWrapper({closeModal, isModalPost=false, children}) 
 
   return createPortal(
     <div className="modal__bg" onClick={(e)=> closeModalWindow(e)}>
-      <div className="modal__body">
+      <div className={cx('modal__body', {'modal__body-login': modalBodyLogIn})}>
         <div className="modal__btnWrapper">
           <RxCross2 className="modal__crossBtn" onClick={closeModal}/>
           {isModalPost && <button className="modal__draftsBtn">{t('btn.drafts')}</button>}
