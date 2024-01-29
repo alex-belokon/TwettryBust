fa29d5bf70f07faa9139033d89943414d3d789a6
Anna Matveeva <107624838+Matveeva-Ann@users.noreply.github.com>
2024-01-29 16:31:11 +0200

    added img modal

--- frontend/src/page/profile/ProfileMedia.jsx
+++ frontend/src/page/profile/ProfileMedia.jsx
@@ -1,8 +1,44 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getUserMedia } from "../../api/profile";
+import ImgModal from "../../components/Modal/ImgModal/ImgModal";
+import "./ProfileMedia.scss";
 
+export default function ProfileMedia() {
+  const [userMedia, setUserMedia] = useState([]);
+  const [isModalImgOpen, setIsModalImgOpen] = useState(false);
+  const [currentImage, setCurrentImage] = useState('')
+  const { id } = useParams();
 
-export default function ProfileMedia(){
+  console.log(userMedia);
 
-  return(
-    <p>ProfileMedia</p>
-  )
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getUserMedia(id);
+        setUserMedia(data);
+      } catch (e) {
+        console.error("Помилка при отриманні даних:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  function openModalImg (elem){
+    setIsModalImgOpen(true)
+    setCurrentImage(elem)
+  }
+   console.log('currentImage', currentImage);
+  return (
+    <>
+      <ul className="media__list">
+        {userMedia.map((elem) => (
+          <li key={elem.imgUrl}>
+            <img className="media__img" src={elem.imgUrl} onClick={()=> openModalImg(elem)}/>
+          </li>
+        ))}
+      </ul>
+      {isModalImgOpen && <ImgModal img={currentImage} setIsModalImgOpen={()=>setIsModalImgOpen(false)}></ImgModal>}
+    </>
+  );
 }
