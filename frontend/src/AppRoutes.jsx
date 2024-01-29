cbbfafb8577f99a040cdbc381bfd93706e561e81
Anna Matveeva <107624838+Matveeva-Ann@users.noreply.github.com>
2024-01-29 11:41:44 +0200

    created post card

--- frontend/src/AppRoutes.jsx
+++ frontend/src/AppRoutes.jsx
@@ -2,14 +2,19 @@ import { Routes, Route } from "react-router-dom";
 import RequireAuth from "./hoc/RequireAuth";
 import Layout from "./Layout/Layout";
 import Home from "./page/Home";
-import Authorization from "./page/Authorization"; // Предполагаем, что у вас есть компонент для страницы логина
-import Profile from "./page/Profile";
+import Authorization from "./page/Authorization";
+import Profile from "./page/profile/Profile";
 import ForgotPassword from "./page/ForgotPassword";
 import Following from "./page/Following";
 import Followers from "./page/Followers";
 import Follow from "./page/Follow";
 import Messages from "./page/Messages/Messages";
 import MessagesDialogSection from "./components/Messages/MessagesDialogSection";
+import ProfilePost from "./page/profile/ProfilePost";
+import ProfileReplies from "./page/profile/ProfileReplies";
+import ProfileHighlights from "./page/profile/ProfileHighlights";
+import ProfileMedia from "./page/profile/ProfileMedia";
+import ProfileLikes from "./page/profile/ProfileLikes";
 
 export default function AppRoutes() {
 
@@ -26,11 +31,11 @@ export default function AppRoutes() {
         <Route path="bookmarks" element={<RequireAuth><Home /></RequireAuth>} />
         <Route path="communities" element={<RequireAuth><Home /></RequireAuth>} />
         <Route path="profile/:id" element={<RequireAuth><Profile /></RequireAuth>}>
-          <Route path="posts" element={<Home />} />
-          <Route path="with_replies" exact element={<Home />} />
-          <Route path="highlights" exact element={<Home />} />
-          <Route path="media" exact element={<Home />} />
-          <Route path="likes" exact element={<Home />} />
+          <Route path="posts" element={<ProfilePost />} />
+          <Route path="with_replies" exact element={<ProfileReplies />} />
+          <Route path="highlights" exact element={<ProfileHighlights />} />
+          <Route path="media" exact element={<ProfileMedia />} />
+          <Route path="likes" exact element={<ProfileLikes />} />
         </Route>
         <Route path="follow" element={<RequireAuth><Follow /></RequireAuth>} >
           <Route path="following" element={<Following />} />
--- frontend/src/components/Posts/Posts.jsx
+++ frontend/src/components/Posts/Posts.jsx
@@ -1,22 +1,94 @@
-import PostCard from "./PostCard/PostCard"
+import { useEffect, useState } from "react";
+import PostCard from "./PostCard/PostCard";
+import "./Posts.scss";
 
-import './Posts.scss'
+export default function Posts({ isFollowingActive }) {
+  const [posts, setPosts] = useState([]);
 
-export default function Posts() {
-    return(
-        <>
-        <div className="post-container">
-            <div className="post-selector-container">
-                <button className="post-selector__btn active">For you</button>
-                <button className="post-selector__btn">Following</button>
-            </div>
+  useEffect(() => {
+    getPosts();
+  }, []);
 
-            <div className="post-create-container">
-                <div className="post-create-area">
-                    <PostCard></PostCard>
-                </div>
-            </div>
-        </div>
-        </>
-    )
+  const url = "http://localhost:5173/";
+
+  async function getPosts() {
+    try {
+      const resp = await fetch(url);
+
+      if (!resp.ok) {
+        throw new Error("Error");
+      }
+
+      // const dataArr = await resp.json();
+
+      const dataArr = [
+        {
+          imgPost:
+            "https://res.cloudinary.com/dfrps0cby/image/upload/v1706292731/ronx3qzcgcif1loe6mor.jpg",
+          userScreensaver:
+            "https://res.cloudinary.com/dfrps0cby/image/upload/v1705663684/samples/smile.jpg",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus inventore illo ratione quo placeat. Veritatis autem unde incidunt iste asperiores.",
+          userName: "QQQQQ",
+          userLastName: "QQQQQ",
+          postDate: new Date(),
+          userLogin: "@login",
+          reply: 1,
+          repost: 0,
+          likes: 555,
+          view: 10000,
+          isInBookmark: true,
+          id: 2,
+        },
+        {
+          imgPost:
+            "https://res.cloudinary.com/dfrps0cby/image/upload/v1705663683/samples/balloons.jpg",
+          userScreensaver:
+            "https://res.cloudinary.com/dfrps0cby/image/upload/v1705663682/samples/two-ladies.jpg",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus inventore illo ratione quo placeat. Veritatis autem unde incidunt iste asperiores.",
+          userName: "userName",
+          userLastName: "userLastName",
+          postDate: new Date(),
+          userLogin: "@login",
+          reply: 2,
+          repost: 10,
+          likes: 5,
+          view: 10,
+          isInBookmark: false,
+          id: 3,
+        },
+        {
+          imgPost:
+            "https://res.cloudinary.com/dfrps0cby/image/upload/v1705663664/samples/bike.jpg",
+          userScreensaver:
+            "https://res.cloudinary.com/dfrps0cby/image/upload/v1705663667/samples/people/bicycle.jpg",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus inventore illo ratione quo placeat. Veritatis autem unde incidunt iste asperiores.",
+          userName: "userName",
+          userLastName: "userLastName",
+          postDate: new Date(),
+          userLogin: "@login",
+          reply: 12,
+          repost: 0,
+          likes: 10,
+          view: 2,
+          isInBookmark: true,
+          id: 4,
+        },
+      ];
+      setPosts(dataArr);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  return (
+    <div className="post-create-container">
+      {isFollowingActive
+        ? posts.map((postData) => (
+            <PostCard postData={postData} key={postData.id} />
+          ))
+        : posts.map((postData) => (
+            <PostCard postData={postData} key={postData.id} />
+          ))}
+    </div>
+  );
 }
