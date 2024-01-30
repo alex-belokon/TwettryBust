cc7e0eb26a429fee1f3fddf9784533dd25810d16
Svitlana2804 <140393790+Svitlana2804@users.noreply.github.com>
2024-01-30 18:42:29 +0200

    Us20 (#56)
    * Initialize React app
    
    * US-20
    
    * US-20
    
    * US20
    
    * US20
    
    * created post card
    
    * created post card
    
    * added img modal
    
    * added img modal
    
    * postPage
    
    * fixed bags
    
    * fixed bags
    
    * develop modalPost
    
    * fixed bag
    
    * fixed bags
    
    * fixed bags
    
    * fixed
    
    ---------
    
    Co-authored-by: Andrew3552 <armbrustrututu66@gmail.com>
    Co-authored-by: GarikVodila <vladislavbelyj12@gmail.com>
    Co-authored-by: GarikVodila <132703736+GarikVodila@users.noreply.github.com>
    Co-authored-by: Anna Matveeva <107624838+Matveeva-Ann@users.noreply.github.com>
    Co-authored-by: Andrii <126251484+Andrew3552@users.noreply.github.com>

---
+++ frontend/src/components/Modal/ModalPost/ModalPost.jsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+// import UserData from "../../Header/UserData/UserData";
+import ModalWrapper from "../ModalElements/ModalWrapper";
+import "./ModalPost.style.scss";
+import ModalBtn from "../../Buttons/ModalBtn/ModalBtn";
+import { RxCross2 } from "react-icons/rx";
+import { useTranslation } from "react-i18next";
+import UploadWidget from "../../UploadWidget";
+import { FcAddImage } from "react-icons/fc";
+import EmojiPicker from "emoji-picker-react";
+import { useSelector } from "react-redux";
+
+export default function ModalPost({ closeModal, bannerUrl, setBannerUrl }) {
+  const { t } = useTranslation();
+  const [postContent, setPostContent] = useState("");
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+  const userData = useSelector((state) => state.authUser.user);
+  const handlePostChange = (e) => {
+    setPostContent(e.target.value);
+  };
+  const handlePostSubmit = () => {
+    // тут має бути POST запит на сервер
+    if (bannerUrl) {
+      setPostContent((prevContent) => prevContent + bannerUrl);
+    }
+    console.log("Опублікувати пост:", postContent);
+    setPostContent("");
+    closeModal();
+  };
+  const handleEmojiClick = (emojiObject) => {
+    const emoji = emojiObject.emoji;
+    setPostContent((prevContent) => prevContent + emoji);
+  };
+  const toggleEmojiPicker = () => {
+    setShowEmojiPicker(!showEmojiPicker);
+  };
+  return (
+    <>
+      <ModalWrapper closeModal={closeModal}>
+        <RxCross2 className="post__crossBtn" onClick={closeModal} />
+        <div className="post__item">
+          {userData.userScreensaver ? (
+            <img
+              className="userData__img"
+              src={userData.userScreensaver}
+              alt={userData.name + " photo"}
+            />
+          ) : (
+            <span className="userData__initials">
+              {`${userData.name}`.split("")[0]}
+            </span>
+          )}
+          <textarea
+            className="textarea"
+            placeholder={t("placeholder.text")}
+            value={postContent}
+            onChange={handlePostChange}
+            rows="4"
+            cols="50"
+          />
+        </div>
+        <div className="post__footer">
+          <ul className="post__list">
+            <li>
+              <div className="tooltip">
+                <UploadWidget imgUrl={setBannerUrl}>
+                  <FcAddImage className="iconAddPost" />
+                </UploadWidget>
+                <p className="tooltip__text">Media</p>
+              </div>
+            </li>
+            <li>
+              <div className="tooltip">
+                <button onClick={toggleEmojiPicker} className="btnEmoji">
+                  🙂
+                </button>
+                {showEmojiPicker && (
+                  <EmojiPicker
+                    onEmojiClick={handleEmojiClick}
+                    disableSearchBar
+                    disableSkinTonePicker
+                  />
+                )}
+                <p className="tooltip__text">Emoji</p>
+              </div>
+            </li>
+          </ul>{" "}
+          <ModalBtn
+            type="button"
+            btnClick={handlePostSubmit}
+            additionalClass="postBtn"
+          >
+            {t(`${"btn.publish"}`)}
+          </ModalBtn>
+        </div>
+      </ModalWrapper>
+    </>
+  );
+}
