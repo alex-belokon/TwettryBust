7e7b389fb352939078d677269db2a4f91d40722c
GarikVodila <132703736+GarikVodila@users.noreply.github.com>
2024-01-31 14:19:13 +0200

    Us20 (#57)
    * Initialize React app
    
    * US-20
    
    * US-20
    
    * US20
    
    * US20
    
    * created post card
    
    * created post card
    
    * added img modal
    
    * added img modal
    
    * postPage
    
    * fixed bags
    
    * fixed bags
    
    * develop modalPost
    
    * fixed bag
    
    * fixed bags
    
    * fixed bags
    
    * fixed
    
    * CreatePost
    
    * CreatePost
    
    * US20
    
    * US20
    
    * added poput to post card
    
    * added poput to post card
    
    ---------
    
    Co-authored-by: Andrew3552 <armbrustrututu66@gmail.com>
    Co-authored-by: Anna Matveeva <107624838+Matveeva-Ann@users.noreply.github.com>
    Co-authored-by: Andrii <126251484+Andrew3552@users.noreply.github.com>
    Co-authored-by: Svitlana Pedchenko <tulipromashka@gmail.com>

--- frontend/src/components/Posts/PostActions/PostActions.jsx
+++ frontend/src/components/Posts/PostActions/PostActions.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { BiMessageRounded, BiRepost, BiBarChart } from "react-icons/bi";
 import { FaRegHeart, FaRegBookmark, FaBookmark } from "react-icons/fa";
+import ModalReply from "../../Modal/ModalReply/ModalReply";
 import "./PostActions.scss";
 
 export default function PostActions({
@@ -10,26 +12,30 @@ export default function PostActions({
   isInBookmark = null,
 }) {
 
+  const [isModalReplyOpen, setIsModalReplyOpen] = useState(false)
+  
+
   return (
     <div className="postCard__bottom">
-      <button className="postCard__iconBtn">
+      <button className="postCard__iconBtn" title="Reply" onClick={()=>setIsModalReplyOpen(true)}>
         <BiMessageRounded />
         <span className="postCard__stats">{reply}</span>
+        {isModalReplyOpen && <ModalReply closeModal={()=>setIsModalReplyOpen(false)}></ModalReply>}
       </button>
-      <button className="postCard__iconBtn postCard__iconBtn--big">
+      <button className="postCard__iconBtn postCard__iconBtn--big postCard__iconBtn--green" title="Repost">
         <BiRepost />
         <span className="postCard__stats">{repost}</span>
       </button>
-      <button className="postCard__iconBtn">
+      <button className="postCard__iconBtn postCard__iconBtn--red" title="Likes">
         <FaRegHeart />
         <span className="postCard__stats">{likes}</span>
       </button>
-      <button className="postCard__iconBtn postCard__iconBtn--big">
+      <button className="postCard__iconBtn postCard__iconBtn--big" title="View">
         <BiBarChart />
         <span className="postCard__stats">{view}</span>
       </button>
       {isInBookmark !== null && (
-        <button className="postCard__iconBtn">
+        <button className="postCard__iconBtn" title="Bookmarks">
           {isInBookmark ? <FaBookmark /> : <FaRegBookmark />}
         </button>
       )}
