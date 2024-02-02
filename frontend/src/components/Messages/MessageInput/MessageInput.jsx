7a5f6da6749feaa21b9b358c4dcc7305df242b1b
Anna Matveeva <107624838+Matveeva-Ann@users.noreply.github.com>
2024-02-02 17:21:10 +0200

    added the ability to add pictures to messages, implemented adaptive design for message window, added emoticon button

--- frontend/src/components/Messages/MessageInput/MessageInput.jsx
+++ frontend/src/components/Messages/MessageInput/MessageInput.jsx
@@ -4,14 +4,22 @@ import { AiOutlinePicture } from "react-icons/ai";
 import { VscSend } from "react-icons/vsc";
 import { useRef, useState } from "react";
 import EmojiPicker from "emoji-picker-react";
+import UploadWidget from "../../UploadWidget";
 
-export default function MessageInput() {
+export default function MessageInput({ setMarginMessageList }) {
   const [showEmojiPicker, setShowEmojiPicker] = useState(false);
-  const [messageContent, setMessageContent] = useState("");
+  const [messageContent, setMessageContent] = useState('');
+  const [imgUrl, setImgUrl] = useState('')
   const textArea = useRef(null);
 
   const toggleEmojiPicker = () => {
     setShowEmojiPicker(!showEmojiPicker);
+  };
+
+  const closeEmojiPicker = (e) => {
+    if (e.target.className === "emojiPicker__wrapper") {
+      setShowEmojiPicker(false);
+    }
   };
   const handleEmojiClick = (emojiObject) => {
     const emoji = emojiObject.emoji;
@@ -19,9 +27,14 @@ export default function MessageInput() {
   };
 
   const textareaInputHandler = (e) => {
-    if (textArea.current && e.target.scrollHeight !== 28) {
+    if (
+      textArea.current &&
+      e.target.scrollHeight !== 28 &&
+      e.target.scrollHeight < 138
+    ) {
       textArea.current.style.height = "auto";
       textArea.current.style.height = `${e.target.scrollHeight}px`;
+      setMarginMessageList(e.target.scrollHeight);
       textArea.current.style.maxHeight = `170px`;
     }
     if (e.target.scrollHeight === 49) {
@@ -36,18 +49,35 @@ export default function MessageInput() {
     <>
       <div className="messageInput__wrapper">
         <div className="messageInput__content">
-          <button className="messageInput__btn">
-            <AiOutlinePicture className="messageInput__icon" />
-          </button>
+            <UploadWidget className='messageInput__btn' imgUrl={setImgUrl}>
+              <AiOutlinePicture className="messageInput__icon" />
+            </UploadWidget>
 
           <button className="messageInput__btn" onClick={toggleEmojiPicker}>
             <FaRegSmileBeam className="messageInput__icon" />
           </button>
+          {showEmojiPicker && (
+            <div
+              className="emojiPicker__wrapper"
+              onClick={(e) => closeEmojiPicker(e)}
+            >
+              <EmojiPicker
+                onEmojiClick={handleEmojiClick}
+                disableSearchBar
+                disableSkinTonePicker
+                style={{ width: "500px" }}
+              />
+            </div>
+          )}
+          {imgUrl && <img className="messageImg" src={imgUrl} />}
+          
+
           <textarea
             type="text"
             className="messageInput__textarea"
             placeholder="Нове повідомлення"
             value={messageContent}
+            onClick={() => setShowEmojiPicker(false)}
             onInput={(e) => textareaInputHandler(e)}
             onChange={handlePostChange}
             ref={textArea}
@@ -57,14 +87,6 @@ export default function MessageInput() {
           </button>
         </div>
       </div>
-      {showEmojiPicker && (
-        <EmojiPicker
-          onEmojiClick={handleEmojiClick}
-          disableSearchBar
-          disableSkinTonePicker
-          style={{ width: "500px" }}
-        />
-      )}
     </>
   );
 }
--- frontend/src/components/Messages/MessagesDialogSection.jsx
+++ frontend/src/components/Messages/MessagesDialogSection.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getUserMessages } from "../../api/messages";
@@ -9,37 +9,73 @@ export default function MessagesDialogSection() {
   const { id } = useParams();
   const currentUserId = useSelector((state) => state.authUser.user.id);
   const [dialog, setDialog] = useState([]);
+  const [marginMessageList, setMarginMessageList] = useState(45);
+  const messageList = useRef(50);
+  const messageContainer = useRef(0);
 
   useEffect(() => {
-    async function fetchData(){
-      try{
+    if (messageList.current) {
+      messageList.current.style.marginBottom = `${marginMessageList * 1.5}px`;
+    }
+  }, [marginMessageList]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
         const data = await getUserMessages(id, currentUserId);
-        setDialog(data)
-      }
-      catch(e){
+        setDialog(data);
+      } catch (e) {
         console.log(e);
       }
     }
     fetchData();
   }, [id]);
 
-  
+  useEffect(() => {
+    if (messageContainer.current) {
+      messageContainer.current.scrollTop = messageList.current.scrollHeight;
+    }
+  }, [dialog]);
 
   return (
-    <div className="messagesDialogSection">
+    <div className="messagesDialogSection" ref={messageContainer}>
       <MessagesDialogHeader></MessagesDialogHeader>
 
-      <ul className="messagesDialogSection__messageList">
+      <ul className="messagesDialogSection__messageList" ref={messageList}>
         {dialog.map((item, index) =>
           item.userId === currentUserId ? (
-            <li className="messagesDialogSection__message" key={index}>{item.message} <span className="messagesDialogSection__date">{new Date(item.date).toLocaleString()}</span></li>
+            <li className="messagesDialogSection__message" key={index}>
+              {item.message}
+              {item.imgUrl && (
+                <img
+                  className="messagesDialog__img"
+                  src={item.imgUrl}
+                  alt="img"
+                />
+              )}
+              <span className="messagesDialogSection__date">
+                {new Date(item.date).toLocaleString()}
+              </span>
+            </li>
           ) : (
-            <li className="messagesDialogSection__message--accent" key={index}>{item.message} <span className="messagesDialogSection__date">{new Date(item.date).toLocaleString()}</span></li>
+            <li className="messagesDialogSection__message--accent" key={index}>
+              {item.message}
+              {item.imgUrl && (
+                <img
+                  className="messagesDialog__img"
+                  src={item.imgUrl}
+                  alt="img"
+                />
+              )}
+              <span className="messagesDialogSection__date">
+                {new Date(item.date).toLocaleString()}
+              </span>
+            </li>
           )
         )}
       </ul>
 
-      <MessageInput></MessageInput>
+      <MessageInput setMarginMessageList={setMarginMessageList}></MessageInput>
     </div>
   );
 }
