9285d48f99ae90932e06c1992c43a75d43a0cd99
Anna Matveeva <107624838+Matveeva-Ann@users.noreply.github.com>
2024-02-12 15:35:09 +0200

    Corrected styles according to linter guidelines

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import { getRecommendUsers } from "../../api/profile";
-import Searching from "../Messages/Searching/Searching";
 import Recommended from "./Recommended/Recommended";
 import { useSelector } from "react-redux";
 import "./Sidebar.scss";
