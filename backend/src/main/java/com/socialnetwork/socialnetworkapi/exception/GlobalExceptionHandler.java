759b72d5381e53062bb11e0f2854fb92197597f5
legio <kirillnosenko304@gmail.com>
2024-02-19 22:16:08 +0200

    Добавлено исключение для входа, и регистрации, теперь они выводятся в json фоормате

--- backend/src/main/java/com/socialnetwork/socialnetworkapi/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/socialnetwork/socialnetworkapi/exception/GlobalExceptionHandler.java
@@ -5,7 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.ModelAndView;
 
 //@ControllerAdvice говорит Spring, что этот класс предназначен для обработки исключений в контроллерах.
 //@ExceptionHandler(Exception.class) указывает метод handleException, чтобы обрабатывать исключения типа Exception.
@@ -28,3 +27,4 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST); // e -> Error DTO, Exception -> Error registration handler
     }
 }
+// Нужно сделать так чтобы ошибки выводились в формате json
