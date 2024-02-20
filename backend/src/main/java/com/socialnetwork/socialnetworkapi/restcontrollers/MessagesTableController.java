02e488c6aa9106552f99f99853d2f4ffacf5e59c
legio <kirillnosenko304@gmail.com>
2024-02-20 14:54:51 +0200

    Добавлены аннотации @Operation к каждому методу в контроллерах API для описания их функциональности

--- backend/src/main/java/com/socialnetwork/socialnetworkapi/restcontrollers/MessagesTableController.java
+++ backend/src/main/java/com/socialnetwork/socialnetworkapi/restcontrollers/MessagesTableController.java
@@ -86,6 +86,7 @@ public class MessagesTableController {
     // Преобразование между DTO и сущностью
     private MessageDTO convertToDTO(Message message) {
         MessageDTO messageDTO = new MessageDTO();
+        messageDTO.setMessageId(message.getId());
         messageDTO.setSenderId(message.getSenderId().getId());
         messageDTO.setContent(message.getContent());
         messageDTO.setDate(message.getDate());
