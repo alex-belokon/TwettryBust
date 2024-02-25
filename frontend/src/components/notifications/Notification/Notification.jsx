8bc63fbca89d281980535a7d4bac6c60855bcf0c
Viktoriia Yevdokimova <yevdokimovaviktoriya8@gmail.com>
2024-02-25 19:10:16 +0100

    Notification

--- frontend/src/components/notifications/Notification/Notification.jsx
+++ frontend/src/components/notifications/Notification/Notification.jsx
@@ -1,13 +1,20 @@
+import Button from "../../Buttons/Button/Button";
 import NotificationWrapper from "../NotificationWrapper/NotificationWrapper";
 import "./Notification.scss"
 
-export default function Notification ({reaction}) {
+export default function Notification ({reaction, posts=[1, 2, 3]}) {
+    const isSingleReaction = posts.length <= 1;
+    const numberPosts = isSingleReaction ? "": posts.length; 
     return <NotificationWrapper reaction={reaction}>
     <div className="notification__content">
         <img className="notification__avatar" src="https://opis-cdn.tinkoffjournal.ru/mercury/main-2-midjourney-avatars.whzewv..jpg?preset=image_570w_2x" alt="" />
-        <p className="notification__follower-name">Vikriya Yevdokimova
-        <span className="notification__reaction">{reaction} your post</span></p>
+        <div className="notification__text-wrapper">
+        <h3 className="notification__follower-name">Vikriya Yevdokimova
+        </h3>
+        <span className="notification__reaction">{reaction} {numberPosts} your post</span>
+        </div>
         <p className="notification__text">text</p>
+        {!isSingleReaction &&<Button className= "notification__btn" onClick={()=>console.log('test')}>Show all</Button>}
         {/* <h3 className="notification__user-name">@Viki_y_Tory</h3> */}
     </div>
     </NotificationWrapper>
