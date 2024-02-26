ee90d0be015c484ecc468cc3d456b6591231cc4b
Andrii <126251484+Andrew3552@users.noreply.github.com>
2024-02-26 18:14:41 +0200

    Us12 (#131)
    * Initialize React app
    
    * add Forgot password
    
    * add Forgot password
    
    * add Forgot password
    
    * registration page
    
    * registration page
    
    * registration page
    
    * fix bug animation madal
    
    * added a message to confirm registration
    
    * added a message to confirm registration
    
    * handling of login errors
    
    * registration error handling
    
    * registration error handling
    
    * solving the problem with not saving user data in LC
    
    * fix bag login
    
    * added deletion of user data from local storage and session storage after closing the window
    
    ---------
    
    Co-authored-by: Anna Matveeva <107624838+Matveeva-Ann@users.noreply.github.com>

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,12 +5,13 @@ import App from "./App.jsx";
 import "./index.css";
 import i18n from "./i18n/i18n";
 
-// window.addEventListener('unload', function() {
-//   const rememberMe = localStorage.getItem('rememberMe') === 'true';
-//   if (!rememberMe) {
-//     localStorage.removeItem('persist:root');
-//   }
-// });
+window.addEventListener('unload', function() {
+  const rememberMe = localStorage.getItem('rememberMe') === 'true';
+  if (!rememberMe) {
+    localStorage.removeItem('persist:user');
+    sessionStorage.removeItem('persist:user');
+  }
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
