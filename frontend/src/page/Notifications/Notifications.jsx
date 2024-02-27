d0e239bba6ebea70facbc2f358e3301923c7303d
Viktoriia Yevdokimova <yevdokimovaviktoriya8@gmail.com>
2024-02-27 22:17:53 +0100

    notification

--- frontend/src/page/Notifications/Notifications.jsx
+++ frontend/src/page/Notifications/Notifications.jsx
@@ -1,15 +1,6 @@
 import { Outlet } from "react-router-dom";
 import ActionsLinkList from "../../components/ActionsLink/ActionsLinkList/ActionsLinkList";
 
-<<<<<<< HEAD
-export default function Notifications () { 
-const tabList = [{text : 'All', path : '/notifications'}, {text : 'Verified', path : '/notifications/verified/'}, {text : 'Mentions', path : '/notifications/mentions/'} ]    
-return <section><div className="sectionSearching__header"><h2 className="sectionSearching__title">Notifications</h2></div>
-    <ActionsLinkList linkList={tabList}/>
-    <Outlet/>
-</section>
-}
-=======
 export default function Notifications() {
   const tabList = [
     { text: "All", path: "/notifications" },
@@ -26,4 +17,3 @@ export default function Notifications() {
     </section>
   );
 }
->>>>>>> main
