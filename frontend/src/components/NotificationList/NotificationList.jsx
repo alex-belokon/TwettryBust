5c2cd68cf76d278d6412f379a8566ba5bd07ada2
Viktoriia Yevdokimova <yevdokimovaviktoriya8@gmail.com>
2024-02-29 20:08:53 +0100

    notification

--- frontend/src/components/NotificationList/NotificationList.jsx
+++ frontend/src/components/NotificationList/NotificationList.jsx
@@ -30,17 +30,17 @@ function getRandom() {
 export default function NotificationList () {
     const [posts, setPosts] = useState(null);////змінити null на [],якщо подив потрібно NotificationEmpty
     const {type} = useParams(); 
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const data = await getPosts('forYou');
-            setPosts(data.sort(getRandom));//закоментувати цю стрічку,якщо подив потрібно NotificationEmpty
-          } catch (error) {
-            console.error("Помилка при отриманні даних:", error);
-          }
-        };
-        fetchData();
-      }, [type]);
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //       try {
+    //         const data = await getPosts('forYou');
+    //         setPosts(data.sort(getRandom));//закоментувати цю стрічку,якщо подив потрібно NotificationEmpty
+    //       } catch (error) {
+    //         console.error("Помилка при отриманні даних:", error);
+    //       }
+    //     };
+    //     fetchData();
+    //   }, [type]);
     const conditionRender = posts && posts.length !== 0
     // return <>{conditionRender ? <PostList  posts= {posts}/> : <NotificationListEmpty type={type}/> }</>
     return <>{conditionRender ? mockNotification.map(element => {return  element.type === 'comments' ?<PostCard postData={element.postData}/> :  <Notification posts = {element.posts} reaction={element.type}/>}) : <NotificationListEmpty type={type}/> }</>
