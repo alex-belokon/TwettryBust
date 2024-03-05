b0b630b5d4a014798db2f55f6afa8a84f8ea9a1a
Svitlana Pedchenko <tulipromashka@gmail.com>
2024-03-05 20:28:25 +0200

    fixed bags

--- frontend/src/components/Modal/Popup/PopupSettings.jsx
+++ frontend/src/components/Modal/Popup/PopupSettings.jsx
@@ -23,7 +23,6 @@ export default function PopupSettings({ closePopup }) {
   }, [theme]);
 
   return (
-<<<<<<< HEAD
     <div style={{ position: "absolute", maxWidth: "320px", width: "100%" }}>
       <Popup closePopup={closePopup}>
         <div className="chooseElemWrapper">
@@ -71,59 +70,6 @@ export default function PopupSettings({ closePopup }) {
             </option>
           </select>
         </div>
-=======
-    <div className="popupSettings__wrapper">
-      <Popup closePopup={closePopup} popupClass='popupClass__width popup'>
-        <ul>
-          <li className="chooseElemWrapper">
-            <label className="chooseTheme__label" htmlFor="select">
-              <LuSunMoon className="popupSettings__icon"/> {t("settings.themeTitle")}
-            </label>
-            <select
-              className="chooseTheme__select"
-              id="select"
-              defaultValue={themeValue}
-              onChange={(event) => setTheme(event.target.value)}
-            >
-              <option className="chooseTheme__option" value="LIGHT">
-                {t("settings.themeLight")}
-              </option>
-              <option className="chooseTheme__option" value="DARK">
-                {t("settings.themeDark")}
-              </option>
-              <option className="chooseTheme__option" value="TURQUOISE">
-                {t("settings.themeTurquoise")}
-              </option>
-              <option className="chooseTheme__option" value="BLUE">
-                {t("settings.themeBlue")}
-              </option>
-            </select>
-          </li>
-          <li className="chooseElemWrapper">
-            <label className="chooseTheme__label" htmlFor="language">
-              <LuLanguages className="popupSettings__icon"/> {t("settings.languageTitle")}
-            </label>
-            <select
-              className="chooseTheme__select"
-              id="language"
-              value={i18n.language || "uk"}
-              onChange={handleChangeLanguage}
-            >
-              <option className="chooseTheme__option" value="en">
-                {t("settings.languageEn")}
-              </option>
-              <option className="chooseTheme__option" value="uk">
-                {t("settings.languageUk")}
-              </option>
-            </select>
-          </li>
-          <li className="chooseElemWrapper">
-            <Link to='/forgot-password' className="changePassword">
-              <TbPasswordUser className="popupSettings__icon"/> Змінити пароль
-            </Link>
-          </li>
-        </ul>
->>>>>>> d7c2acb851fa2fd7553a799da3d1a02e42ce78af
       </Popup>
     </div>
   );
