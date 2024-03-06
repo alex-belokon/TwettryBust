f97a55c37734030a1477caa555dd5529b5e28c0e
Anna Matveeva <107624838+Matveeva-Ann@users.noreply.github.com>
2024-03-06 16:02:28 +0200

    added deleted code

--- frontend/src/components/Posts/PostActions/PostActions.jsx
+++ frontend/src/components/Posts/PostActions/PostActions.jsx
@@ -60,7 +60,10 @@ export default function PostActions({
 
   function isRepost () {
     if (postData.originalPost && postData.author.id === currentUserId) {
+      setIsRepostCurrentUser (true);
+      setIsDisabled(true);
     } else if (!postData.originalPost  && postData.author.id === currentUserId) {
+      setIsRepostCurrentUser (true);
       setIsDisabled(true);
     } else {
       setIsRepostCurrentUser (false);
@@ -89,7 +92,7 @@ export default function PostActions({
           className="postCard__iconBtn postCard__iconBtn--big postCard__iconBtn--green"
           title="Repost"
           onClick={() => setIsPopupRepostOpen(true)}
-          disabled={isRepostCurrentUser}
+          disabled={isDisabled}
         >
           {isRepostCurrentUser ? <BiRepost style={{ color: "#4b8f23"}}/> : <BiRepost />}
           <span className="postCard__stats">
