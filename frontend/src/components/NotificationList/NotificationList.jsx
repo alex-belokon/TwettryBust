f4c1cd3536f110e589ed6c631ac7e0b7b182cbfc
Viktoriia Yevdokimova <yevdokimovaviktoriya8@gmail.com>
2024-03-07 10:56:45 +0100

    Notification

--- frontend/src/components/NotificationList/NotificationList.jsx
+++ frontend/src/components/NotificationList/NotificationList.jsx
@@ -1,36 +1,47 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import NotificationListEmpty from "./NotificationListEmpty/NotificationListEmpty";
 import Notification from "../notifications/Notification/Notification";
 import PostCard from "../Posts/PostCard/PostCard";
-import "../notifications/Notification/Notification.scss"
-import "./notificationList.scss"
+import "../notifications/Notification/Notification.scss";
+import "./notificationList.scss";
 import { getNotifications } from "../../api/notification";
 
-export default function NotificationList () {
-    const [posts, setPosts] = useState(null);
-    const {type} = useParams(); 
-    useEffect(() => { getNotifications()
-        const fetchData = async () => {
-          try { 
-            const data = await getNotifications();
-            setPosts(data); 
-          } catch (error) {
-            console.error("Помилка при отриманні даних:", error);
-          }
-        };
-        fetchData();
-      }, []);
-    const conditionRender = posts && posts.length !== 0
-    return <>{conditionRender ? posts.map(element => {return  element.notificationType === 'comments' ?<PostCard postData={element.postData}/> :  <Notification posts = {element.posts} reaction={element.notificationType} data={element}/>}) : <NotificationListEmpty type={type}/> } </>
+export default function NotificationList() {
+  const [posts, setPosts] = useState(null);
+  const { type } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getNotifications();
+        setPosts(data);
+      } catch (error) {
+        console.error("Помилка при отриманні даних:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const conditionRender = posts && posts.length !== 0;
+
+  return (
+    <>
+      {conditionRender ? (
+        posts.map((element) =>
+          element.notificationType === "comments" ? (
+            <PostCard postData={element.postData} />
+          ) : (
+            <Notification
+              posts={element.posts}
+              reaction={element.notificationType}
+              data={element}
+            />
+          )
+        )
+      ) : (
+        <NotificationListEmpty type={type} />
+      )}
+    </>
+  );
 }
-/* WebSocket */
-    // const socket = useRef(new WebSocket ("ws://localhost:9000/api/notifications"));
-    // function conecting () {
-    //   socket.current.onopen = function(e) {
-    //     alert("[open] Соединение установлено");
-    //   };
-    // }
-    // useEffect(() => {
-    //   conecting()
-    // }, []);
