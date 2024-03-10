38c451588347d2f997739b2ded37d6980b8264cd
recryogen <acid_surtr@proton.me>
2024-03-10 12:20:50 +0200

    UserId through Auth Token implement.

--- backend/src/main/java/com/socialnetwork/socialnetworkapi/restcontrollers/PostsController.java
+++ backend/src/main/java/com/socialnetwork/socialnetworkapi/restcontrollers/PostsController.java
@@ -1,129 +1,150 @@
 package com.socialnetwork.socialnetworkapi.restcontrollers;
 
 import com.socialnetwork.socialnetworkapi.dao.repository.PostRepository;
+import com.socialnetwork.socialnetworkapi.dao.repository.UserRepository;
 import com.socialnetwork.socialnetworkapi.dto.favAndLikes.FavoriteToggleRequest;
 import com.socialnetwork.socialnetworkapi.dto.favAndLikes.LikeRequest;
 import com.socialnetwork.socialnetworkapi.dto.post.PostRequest;
 import com.socialnetwork.socialnetworkapi.dto.post.PostResponseFull;
 import com.socialnetwork.socialnetworkapi.dto.user.PageReq;
+import com.socialnetwork.socialnetworkapi.model.AbstractEntity;
 import com.socialnetwork.socialnetworkapi.model.Post;
 import com.socialnetwork.socialnetworkapi.service.FavsAndLikesService;
 import com.socialnetwork.socialnetworkapi.service.PostService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@RestController @Slf4j
+@RestController
+@Slf4j
 @RequestMapping(path = "/api/posts")
+@Tag(name = "Ендпоінти постів (не автозгенеровані)")
 public class PostsController {
     private final PostService postService;
     private final PostRepository postRepository;
     private final FavsAndLikesService favsAndLikesService;
+    private final UserRepository userRepository;
 
-    public PostsController(PostService postService, PostRepository postRepository, FavsAndLikesService favsAndLikesService) {
+    private UUID getUserIdByUserDetails(UserDetails userDetails){
+        return userRepository.findByUserName(userDetails.getUsername()).map(AbstractEntity::getId).orElse(null);
+    }
+
+    public PostsController(PostService postService, PostRepository postRepository, FavsAndLikesService favsAndLikesService, UserRepository userRepo) {
         this.postService = postService;
         this.postRepository = postRepository;
         this.favsAndLikesService = favsAndLikesService;
+        this.userRepository = userRepo;
     }
 
-    /**
-     * Get Endpoints:
-     * getAll
-     * geyForId
-     * getById
-     * getLikedBy
-     * @return List of PostResponseShort, may return FullDTO (byId)
-     */
-
     @Operation(summary = "Получение всех постов")
-    @GetMapping("/") public ResponseEntity<List<PostResponseFull>> getAll(@RequestParam UUID uid, @RequestParam Integer page){
-        PageReq req = new PageReq(uid, page);
+    @GetMapping("/")
+    public ResponseEntity<List<PostResponseFull>> getAll(@AuthenticationPrincipal UserDetails userDetails, @RequestParam Integer page) {
+        PageReq req = new PageReq(getUserIdByUserDetails(userDetails), page);
         List<PostResponseFull> resp = postService.getFullDTOlist(req);
         return resp != null
                 ? new ResponseEntity<>(resp, HttpStatus.OK)
                 : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
+
     @Operation(summary = "Получение поста по его идентификатору")
-    @GetMapping("/{id}") public ResponseEntity<PostResponseFull> getById(@PathVariable UUID id, @RequestParam UUID currentUserId){
-        PostResponseFull resp = postService.getById(id, currentUserId);
-        return resp!= null
+    @GetMapping("/{id}")
+    public ResponseEntity<PostResponseFull> getById(@PathVariable UUID id, @AuthenticationPrincipal UserDetails userDetails) {
+        PostResponseFull resp = postService.getById(id, getUserIdByUserDetails(userDetails));
+        return resp != null
                 ? new ResponseEntity<>(resp, HttpStatus.OK)
                 : new ResponseEntity<>(HttpStatus.NOT_FOUND);
 
     }
 
     @Operation(summary = "Получение списка постов, которые понравились пользователю с указанным идентификатором")
-    @GetMapping("/likedBy") public ResponseEntity<List<PostResponseFull>> getLikedBy(@RequestParam UUID uid, @RequestParam Integer page){
-        PageReq req = new PageReq(uid, page);
+    @GetMapping("/likedBy")
+    public ResponseEntity<List<PostResponseFull>> getLikedBy(@AuthenticationPrincipal UserDetails userDetails, @RequestParam Integer page) {
+        PageReq req = new PageReq(getUserIdByUserDetails(userDetails), page);
         List<PostResponseFull> resp = postService.getLikedBy(req);
-        return resp!= null
-               ? new ResponseEntity<>(resp, HttpStatus.OK)
+        return resp != null
+                ? new ResponseEntity<>(resp, HttpStatus.OK)
                 : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
+
     @Operation(summary = "Получение списка постов, которые добавлены в избранное пользователем с указанным идентификатором")
-    @GetMapping("/favoredBy") public ResponseEntity<List<PostResponseFull>> getFavoredBy(@RequestParam UUID uid, @RequestParam Integer page){
-        PageReq req = new PageReq(uid, page);
+    @GetMapping("/favoredBy")
+    public ResponseEntity<List<PostResponseFull>> getFavoredBy(@AuthenticationPrincipal UserDetails userDetails, @RequestParam Integer page) {
+        PageReq req = new PageReq(getUserIdByUserDetails(userDetails), page);
         List<PostResponseFull> resp = postService.getFavoredBy(req);
-        return resp!= null
-              ? new ResponseEntity<>(resp, HttpStatus.OK)
+        return resp != null
+                ? new ResponseEntity<>(resp, HttpStatus.OK)
                 : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
-    @GetMapping("/followedUsersPosts") public ResponseEntity<List<PostResponseFull>> getFollowedUsersPosts(@RequestParam UUID uid, @RequestParam Integer page){
-        PageReq req = new PageReq(uid, page);
+
+    @GetMapping("/followedUsersPosts")
+    public ResponseEntity<List<PostResponseFull>> getFollowedUsersPosts(@AuthenticationPrincipal UserDetails userDetails, @RequestParam Integer page) {
+        PageReq req = new PageReq(getUserIdByUserDetails(userDetails), page);
         List<PostResponseFull> resp = postService.getFollowedUsersPosts(req);
-        return resp!= null
+        return resp != null
                 ? new ResponseEntity<>(resp, HttpStatus.OK)
                 : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     /**
      * Post Endpoints: save edit
+     *
      * @return Post Response full
      */
     @Operation(summary = "Создание нового поста")
-   @PostMapping("/") public ResponseEntity<PostResponseFull> save(@RequestBody PostRequest request) {
-       PostResponseFull resp = postService.save(request);
-       return resp != null
-               ? new ResponseEntity<>(resp, HttpStatus.OK)
-               : new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-   }
+    @PostMapping("/")
+    public ResponseEntity<PostResponseFull> save(@AuthenticationPrincipal UserDetails userDetails, @RequestBody PostRequest request) {
+        PostResponseFull resp = postService.save(getUserIdByUserDetails(userDetails), request);
+        return resp != null
+                ? new ResponseEntity<>(resp, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
 
     @Operation(summary = "Переключение статуса понравившегося для поста")
-    @PostMapping("/favorite") public ResponseEntity<Boolean> toggleFavorite(@RequestBody FavoriteToggleRequest req){
-       return favsAndLikesService.toggleFavorite(req)
-               ? new ResponseEntity<>(true, HttpStatus.OK)
-               : new ResponseEntity<>(false, HttpStatus.BAD_REQUEST);
-   }
+    @PostMapping("/favorite")
+    public ResponseEntity<Boolean> toggleFavorite(@AuthenticationPrincipal UserDetails userDetails,UUID postId) {
+        return favsAndLikesService.toggleFavorite(new FavoriteToggleRequest(getUserIdByUserDetails(userDetails), postId))
+                ? new ResponseEntity<>(true, HttpStatus.OK)
+                : new ResponseEntity<>(false, HttpStatus.BAD_REQUEST);
+    }
+
     @Operation(summary = "Переключение статуса лайка для поста")
-    @PostMapping("/like") public ResponseEntity<Boolean> toggleLike(@RequestBody LikeRequest req){
-       return favsAndLikesService.toggleLike(req)
-             ? new ResponseEntity<>(true, HttpStatus.OK)
-               : new ResponseEntity<>(false, HttpStatus.BAD_REQUEST);
-   }
+    @PostMapping("/like")
+    public ResponseEntity<Boolean> toggleLike(@AuthenticationPrincipal UserDetails userDetails,UUID postId) {
+        return favsAndLikesService.toggleLike(new LikeRequest(getUserIdByUserDetails(userDetails), postId))
+                ? new ResponseEntity<>(true, HttpStatus.OK)
+                : new ResponseEntity<>(false, HttpStatus.BAD_REQUEST);
+    }
+
     @Operation(summary = "Редактирование поста по его идентификатору")
-    @PutMapping("/edit/{id}") public ResponseEntity<PostResponseFull> editUser(@PathVariable UUID id, @RequestBody PostRequest req){
+    @PutMapping("/edit/{id}")
+    public ResponseEntity<PostResponseFull> editPost(@AuthenticationPrincipal UserDetails userDetails, @PathVariable UUID id, @RequestBody PostRequest req) {
         try {
-            PostResponseFull resp = postService.edit(id, req);
+            PostResponseFull resp = postService.edit(getUserIdByUserDetails(userDetails), id, req);
             if (resp == null) throw new Exception("Invalid ID");
             return new ResponseEntity<>(resp, HttpStatus.OK);
-        }catch (Exception ex){
+        } catch (Exception ex) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
+
     @Operation(summary = "Удаление поста по его идентификатору")
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteById(@PathVariable(name = "id") UUID id) {
-        final boolean result = postService.deletePost(id);
+    public ResponseEntity<?> deleteById(@AuthenticationPrincipal UserDetails userDetails, @PathVariable(name = "id") UUID id) {
+        final boolean result = postService.deletePost(getUserIdByUserDetails(userDetails) ,id);
         return result
                 ? new ResponseEntity<>(HttpStatus.OK)
                 : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
     }
+
     @Operation(summary = "Зберегти лайк на пості")
     @PostMapping
     public ResponseEntity<?> saveLikeOnPost(@RequestParam("postId") UUID postId, @RequestParam("userId") UUID userId) {
