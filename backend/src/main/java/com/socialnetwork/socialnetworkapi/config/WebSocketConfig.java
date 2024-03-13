92d3ad5a8cedc74d9abac765d0ce0d40b4d408e1
legio <kirillnosenko304@gmail.com>
2024-03-13 18:37:44 +0200

    Websockets

--- backend/src/main/java/com/socialnetwork/socialnetworkapi/config/WebSocketConfig.java
+++ backend/src/main/java/com/socialnetwork/socialnetworkapi/config/WebSocketConfig.java
@@ -20,7 +20,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/gs-guide-websocket").setAllowedOrigins("*").withSockJS();
-
     }
 
 }
