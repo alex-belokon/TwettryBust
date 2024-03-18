91f721c9166145c5c57325e5213bc62bdc70ca6a
legio <kirillnosenko304@gmail.com>
2024-03-18 13:00:03 +0200

    /chat/{userId}

--- backend/src/main/java/com/socialnetwork/socialnetworkapi/config/WebSocketConfig.java
+++ backend/src/main/java/com/socialnetwork/socialnetworkapi/config/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic", "/queue", "/chat", "/hello", "/chat/{chatId}");
+        config.enableSimpleBroker("/topic", "/queue", "/chat", "/hello", "/chat/{chatId}, /chat/{userId}");
         config.setApplicationDestinationPrefixes("/app");
     }
 
