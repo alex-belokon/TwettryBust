0a8e3bd0f0eedcb63a75685c553acaefafa8f19a
Andrii <126251484+Andrew3552@users.noreply.github.com>
2024-03-21 09:56:13 +0200

    Us20 comments (#222)
    * Initialize React app
    
    * fix login bag
    
    * Adding, displaying and deleting comments has been implemented
    
    * fix bag Login
    
    * fix bug comment
    
    * fix bug
    
    * fix comment
    
    ---------
    
    Co-authored-by: Anna Matveeva <107624838+Matveeva-Ann@users.noreply.github.com>

--- frontend/src/api/posts.js
+++ frontend/src/api/posts.js
@@ -207,7 +207,7 @@ export const postCommentPost = async (postId, comment) => {
 export const fetchComments = async (id, page = 0) => {
   const token = JSON.parse(userToken());
   try {
-    const response = await fetch(`${baseUrl}/posts/${id}/comments?page=${page}&size=5`, {
+    const response = await fetch(`${baseUrl}/posts/${id}/comments?page=${page}&size=10`, {
       headers: {
         'Authorization': `Bearer ${token}`,
       },
