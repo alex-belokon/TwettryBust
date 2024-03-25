b16c683bc33c20756113fdd00bd66376fd6dcf05
Christian <156912716+recryogen@users.noreply.github.com>
2024-03-25 21:05:31 +0200

    main merge attempt

--- backend/src/main/java/com/socialnetwork/socialnetworkapi/dto/notification/NotificationResponse.java
+++ backend/src/main/java/com/socialnetwork/socialnetworkapi/dto/notification/NotificationResponse.java
@@ -1,7 +1,7 @@
 package com.socialnetwork.socialnetworkapi.dto.notification;
 
 import com.socialnetwork.socialnetworkapi.enums.NotificationType;
-import com.socialnetwork.socialnetworkapi.model.Post;
+import com.socialnetwork.socialnetworkapi.model.Notification;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,15 +10,25 @@ import java.util.UUID;
 @Getter
 @Setter
 @Schema(name = "NotificationDto")
-public class NotificationDto {
+public class NotificationResponse {
     @Schema(name = "id, уникальный идентификатор поста")
     private UUID postId;
     @Schema(name = "id, уникальный идентификатор уведомления")
     private UUID notificationId;
     @Schema(name = "receiver, пользователь, который получил уведомление")
-    private UUID receiver;
+    private UUID recipientId;
     @Schema(name = "sender, пользователь, который отправил уведомление")
-    private UUID sender;
+    private UUID senderId;
     @Schema(name = "notificationType, тип уведомления")
     private NotificationType notificationType;
+    @Schema(name = "isRead, чи продивлене сповіщення")
+    private Boolean isRead;
+    public NotificationResponse(Notification notification){
+        this.postId = notification.getPostId();
+        this.notificationId = notification.getId();
+        this.recipientId = notification.getRecipient().getId();
+        this.senderId = notification.getSender().getId();
+        this.notificationType = notification.getNotificationType();
+        this.isRead = notification.isRead();
+    }
 }
