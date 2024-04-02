e3306603c6f4271836861ece4ceecc365f4870ac
Christian <156912716+recryogen@users.noreply.github.com>
2024-04-02 16:25:51 +0300

    Revert "main merge attempt (#230)"
    This reverts commit 79a7a81221fff9ab82ff5e9d55eabc7e20c3c927.

--- backend/src/main/java/com/socialnetwork/socialnetworkapi/service/DefaultNotificationService.java
+++ backend/src/main/java/com/socialnetwork/socialnetworkapi/service/DefaultNotificationService.java
@@ -3,20 +3,12 @@ package com.socialnetwork.socialnetworkapi.service;
 import com.socialnetwork.socialnetworkapi.dao.repository.NotificationRepository;
 import com.socialnetwork.socialnetworkapi.dao.repository.PostRepository;
 import com.socialnetwork.socialnetworkapi.dao.service.NotificationService;
-import com.socialnetwork.socialnetworkapi.dto.notification.NotificationDto;
-import com.socialnetwork.socialnetworkapi.dto.notification.NotificationResponse;
 import com.socialnetwork.socialnetworkapi.enums.NotificationType;
 import com.socialnetwork.socialnetworkapi.exception.BadRequestException;
-import com.socialnetwork.socialnetworkapi.exception.NotImplementedEx;
 import com.socialnetwork.socialnetworkapi.model.Notification;
 import com.socialnetwork.socialnetworkapi.model.Post;
 import com.socialnetwork.socialnetworkapi.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -28,14 +20,11 @@ public class DefaultNotificationService extends NotificationService {
 
     private final NotificationRepository notificationRepository;
     private final PostRepository postRepository;
-    private final SimpMessagingTemplate messagingTemplate;
 
-    private final Integer pageSize = 20;
     @Autowired
-    public DefaultNotificationService(NotificationRepository notificationRepository, PostRepository postRepository, SimpMessagingTemplate messagingTemplate) {
+    public DefaultNotificationService(NotificationRepository notificationRepository, PostRepository postRepository) {
         this.notificationRepository = notificationRepository;
         this.postRepository = postRepository;
-        this.messagingTemplate = messagingTemplate;
     }
 
     @Override
@@ -45,6 +34,8 @@ public class DefaultNotificationService extends NotificationService {
         if (sender.isEmpty() || recipient.isEmpty()) {
             throw new BadRequestException("Sender or recipient not found");
         }
+        // Проверяем, существует ли пост с указанным идентификатором
+        Post optionalPost = postRepository.getPostById(postId);
 
 
         Notification notification = new Notification();
@@ -52,36 +43,13 @@ public class DefaultNotificationService extends NotificationService {
         sender.ifPresent(notification::setSender);
         recipient.ifPresent(notification::setRecipient);
         notification.setRead(false);
-        notification.setPostId(postId);
+        notification.setPost(optionalPost);
         return notificationRepository.save(notification);
     }
-    @Override
-    public NotificationResponse createAndMapNotification(Optional<User> sender, Optional<User> recipient, NotificationType notificationType, UUID postId){
-        Notification notification = createNotification(sender, recipient, notificationType, postId);
-        return new NotificationResponse(notification);
-    }
 
-    @Override
-    public NotificationResponse createAndNotify(Optional<User> sender, Optional<User> recipient, NotificationType notificationType, UUID postId){
-        NotificationResponse response = this.createAndMapNotification(sender, recipient, notificationType, postId);
-        this.notifyUser(response);
-        return response;
-    }
     @Override
     // Метод для получения всех уведомлений для конкретного пользователя
-    public List<Notification> getAllNotificationsForUser(Optional<User> recipient, Integer page) {
-        Pageable pageable =  PageRequest.of(page, this.pageSize, Sort.by("createdAt").descending());
-        return notificationRepository.findByRecipient(recipient, pageable);
+    public List<Notification> getAllNotificationsForUser(Optional<User> recipient) {
+        return notificationRepository.findByRecipient(recipient);
     }
-
-    private void notifyUser(NotificationResponse notification){
-//        messagingTemplate.convertAndSendToUser(notification.getRecipientId().toString(), "/topic/notification", notification); // "/{id}/topic/notification"
-        messagingTemplate.convertAndSend("/"+notification.getRecipientId()+"/notification", notification);
-    }
-
-    @Override
-    public Boolean toggleIsRead(UUID userId, UUID notificationId){
-        throw new NotImplementedEx("METHOD DefaultNotificationService.toggleIsRead CURRENTLY UNSUPPORTED");
-    }
-
 }
