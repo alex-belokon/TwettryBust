066d9b13cf65ac9a6e3ec15555a030bf56545068
ViktoriyaYE <138582023+ViktoriyaYE@users.noreply.github.com>
2024-04-08 18:06:25 +0200

    Noty actual t (#246)
    * Add page notifications and other components for this page
    
    * Merge request
    
    * new branch
    
    * new push
    
    * fix again
    
    * notification fix
    
    * fix notifications again
    
    * fix again
    
    * fix again
    
    * fix
    
    * fix notification again
    
    * Start notification
    
    * notification
    
    * Notification
    
    * Notification
    
    * notification
    
    * notification
    
    * notification
    
    * notification
    
    * notification
    
    * Notification
    
    * Notification
    
    * Notification
    
    * Notification
    
    * Notif
    
    * Notif
    
    * notif
    
    * Notif
    
    * Notif
    
    * Notification
    
    * Notif
    
    * Notif
    
    * Notification
    
    * Notification
    
    * Noty
    
    * Noty data
    
    * Noty
    
    * Noty merge
    
    * Noty Merge
    
    * NotyMerge
    
    * Noty merge
    
    * NotyMerge
    
    * Noty
    
    * NotyMerge
    
    * notyWebs
    
    * NotyWebsokit
    
    * NotyWebsok
    
    * NotyWebs
    
    * notySkeleton
    
    * notySoket
    
    * notyWeb
    
    * NotyWebSok
    
    * NotyBug
    
    * Noty comments
    
    * noty continue
    
    * NotyActual
    
    * NotyActualTT
    
    * noty margeT
    
    * NotyT
    
    ---------
    
    Co-authored-by: Bohdan Admin <bohdanadm1n@gmail.com>
    Co-authored-by: Anna Matveeva <107624838+Matveeva-Ann@users.noreply.github.com>

--- frontend/src/components/notifications/NotificationWrapper/Function.jsx
+++ frontend/src/components/notifications/NotificationWrapper/Function.jsx
@@ -1,5 +1,5 @@
 import { BiRepost, BiSolidHeart } from "react-icons/bi";
 export function getRightIcon (type) {
-    switch(type){case 'repost': return <BiRepost className="notification__repost"/>; 
-    case 'like': return <BiSolidHeart className="notification__heart"/>};
+    switch(type){case 'notification.repost': return <BiRepost className="notification__repost"/>; 
+    case 'notification.like': return <BiSolidHeart className="notification__heart"/>};
 } 
